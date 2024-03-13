3551667de8335de48fe54ca817d40b889fa85b4d
hangyeol0531 <jhan0531@naver.com>
2024-03-14 00:38:45 +0900

    spring data jpa - native query

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -366,7 +366,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void projections(){
+    public void projections() {
         //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
@@ -387,6 +387,29 @@ class MemberRepositoryTest {
             System.out.println("username = " + username);
             System.out.println("teamName = " + teamName);
         }
+    }
 
+    @Test
+    public void nativeQuery() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getId() = " + memberProjection.getId());
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
     }
 }
